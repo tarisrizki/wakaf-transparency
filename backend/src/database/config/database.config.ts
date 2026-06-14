40b073321a55446aeba48059bc4cfcb00e8de9b2
Dijokiaja <deploy@dijokiaja.com>
2026-06-14 18:35:07 +0700

    fix: Bypass failing migration script on Railway by forcing TypeORM synchronize

--- backend/src/database/config/database.config.ts
+++ backend/src/database/config/database.config.ts
@@ -86,7 +86,7 @@ export default registerAs<DatabaseConfig>('database', () => {
     password: process.env.DATABASE_PASSWORD,
     name: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
-    synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+    synchronize: true, // process.env.DATABASE_SYNCHRONIZE === 'true',
     maxConnections: process.env.DATABASE_MAX_CONNECTIONS
       ? parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10)
       : 100,
